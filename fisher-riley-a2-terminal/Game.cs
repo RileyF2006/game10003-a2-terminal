fe0277e2de2c76da595475b5775298fd0a8ae5fd
catlo <catlo@RILEYSLAPTOP>
2024-10-11 02:32:23 -0400

    Update code

--- fisher-riley-a2-terminal/Game.cs
+++ fisher-riley-a2-terminal/Game.cs
@@ -1,4 +1,5 @@
 ﻿// Include code libraries you need below (use the namespace).
+using Raylib_cs;
 using System;
 using System.Numerics;
 
@@ -11,14 +12,23 @@ namespace Game10003
     public class Game
     {
         // Place your variables here:
-
+        private Color color;
+        private Color fillColor;
+        private int eyePlacement1;
+        private int eyePlacement2;
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
+            Window.SetTitle("Interactive Terminal");
+            Window.SetSize(800, 600);
 
+            color = Random.Color();
+            fillColor = Color.OffWhite;
+            eyePlacement1 = Random.Integer(250, 350);
+            eyePlacement2 = Random.Integer(400, 650);
         }
 
         /// <summary>
@@ -26,6 +36,27 @@ namespace Game10003
         /// </summary>
         public void Update()
         {
+
+
+            Window.ClearBackground(Color.OffWhite);
+
+            Draw.LineSize = 2;
+            Draw.FillColor = fillColor;
+
+            if (Input.IsMouseButtonDown(MouseInput.Left))
+            {
+                Draw.FillColor = color;
+            }
+
+            Draw.Circle(400, 300, 250);
+
+            Draw.Circle(eyePlacement1, 250, 75);
+            Draw.Circle(eyePlacement1, 250, 50);
+
+            Draw.Circle(eyePlacement2, 250, 75);
+            Draw.Circle(eyePlacement2, 250, 50);
+
+            Draw.Ellipse(400, 400, 150, 75);
 
         }
     }
